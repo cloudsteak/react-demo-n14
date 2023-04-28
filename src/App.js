27b93b4a02c869ea40ccbcfb8b2eec15c19c72e4
Tibor Kiss <the1bithu@gmail.com>
2023-04-28 16:38:57 +0200

    APP

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.png';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,10 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <a href="https://gerillamentorklub.hu" target="_blank">Tanulj a Gerilla Mentor Klub képzésein!</a>
-
     </div>
   );
 }
